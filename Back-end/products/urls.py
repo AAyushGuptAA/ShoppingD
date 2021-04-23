aa69a892cd000d2bb6b050948b2e32c2291f0c4c
AAyushGuptAA <58947049+AAyushGuptAA@users.noreply.github.com>
2021-04-23 14:55:07 +0530

    Delete Back-end directory

--- Back-end/products/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("best/", views.best_sellers, name="Best_Sellers"),
-    path("men/", views.men, name="Men Items"),
-    path("women/", views.women, name="Women Items"),
-    path("hat/", views.hats, name="All Hats")
-]
